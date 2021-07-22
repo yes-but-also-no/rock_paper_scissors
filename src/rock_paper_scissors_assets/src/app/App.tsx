a4222348f825ec9ce358bfda3b15776b099e8e3b
yes-but-also-no <16858421+yes-but-also-no@users.noreply.github.com>
2021-07-22 07:00:25 -0700

    react basics working with typescript and webpack

---
+++ src/rock_paper_scissors_assets/src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Dfinity imports
+import { rock_paper_scissors } from "../../../declarations/rock_paper_scissors";
+
+const App: React.FC = () => {
+
+    return (
+        <h1>Hello World!</h1>
+    );
+};
+
+export default App;
--- src/rock_paper_scissors_assets/src/index.js
+++
@@ -1,9 +0,0 @@
-import { rock_paper_scissors } from "../../declarations/rock_paper_scissors";
-
-document.getElementById("clickMeBtn").addEventListener("click", async () => {
-  const name = document.getElementById("name").value.toString();
-  // Interact with rock_paper_scissors actor, calling the greet method
-  const result = await rock_paper_scissors.playMove({rock: null}, [name]);
-
-  console.log(result);
-});
