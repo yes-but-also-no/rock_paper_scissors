c66f97bf2a9f2f9b2cfc47980ac8fde8c4006426
yes-but-also-no <16858421+yes-but-also-no@users.noreply.github.com>
2021-07-23 15:00:03 -0700

    update logo styles

--- src/rock_paper_scissors_assets/src/components/Logo.tsx
+++ src/rock_paper_scissors_assets/src/components/Logo.tsx
@@ -1,35 +1,34 @@
 import React from "react";
 import styled from "styled-components";
 
-// The logo container has styling for our logo
-const LogoContainer = styled.div`
+// The logo contains styling and text for the logo
+const Logo = styled.div`
+
+  // size font based on view height
+  font-size: 8vh;
   
-  // font
-  font-family: game-font-bold, sans-serif;
-  
-  // backup for fallback font
-  font-weight: bold;
-  
-  // sizing
-  font-size: 2em;
-  
-  // center
+  // center align
   text-align: center;
   
-  // color
-  color: var(--color-orange);
+  // cool, comic book yellow
+  color: var(--color-yellow);
   
-  // outline
+  // logo font or sans
+  font-family: logo-font, sans-serif;
+  
+  // this prevents the 'k' and 'r' from overlapping the commas
+  letter-spacing: 4px;
+  
+  // outline effect
   -webkit-text-stroke: 2px black;
   
+  // shaddow effect
+  text-shadow: 2px 4px 0 black;
+  
+  &:after {
+    content: 'Block, Paper, Scissors!'
+  }
 `;
 
-// actual logo component
-const Logo: React.FC = () =>
-    <LogoContainer>
-        Block, Paper,
-        <br />
-        Scissors!
-    </LogoContainer>
-
 export default Logo;
+
