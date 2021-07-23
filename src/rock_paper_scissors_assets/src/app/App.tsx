f65f66edb3d9179114ebc8df165db0e0bf80e683
yes-but-also-no <16858421+yes-but-also-no@users.noreply.github.com>
2021-07-23 16:37:52 -0700

    setup basic game layout

--- src/rock_paper_scissors_assets/src/app/App.tsx
+++ src/rock_paper_scissors_assets/src/app/App.tsx
@@ -1,23 +1,52 @@
 import React from 'react';
-import { Grid, Row, Col } from 'react-flexbox-grid';
 import styled from "styled-components";
 import Logo from "../components/Logo";
 
-const TestCol = styled(Col)`
-    border: 1px solid red;
-    max-width: 800px;
-`
+// Main column controls our width, and ensures we are using the full height of the screen
+const MainContentArea = styled.div`
+  // prevent hell on ultra-wides
+  max-width: 768px;
 
+  // fill the screen
+  height: 100vh;
+
+  // center
+  margin: auto;
+
+  // weird flex, but okay
+  display: flex;
+
+  // layout all the pieces
+  flex-direction: column;
+
+  // space between to make it nice
+  justify-content: space-between;
+`;
+
+// sections fill their contents
+const Section = styled.div`
+  flex: 0;
+`;
+
+// Main app component
 const App: React.FC = () => {
 
     return (
-        <Grid fluid>
-            <Row center='xs'>
-                <TestCol xs={12} md={8} lg={8}>
-                    <Logo />
-                </TestCol>
-            </Row>
-        </Grid>
+        <MainContentArea>
+
+            <Section>
+                <Logo/>
+            </Section>
+
+            <Section>
+                <Logo/>
+            </Section>
+
+            <Section>
+                <Logo/>
+            </Section>
+
+        </MainContentArea>
     );
 };
 
--- src/rock_paper_scissors_assets/src/components/Logo.tsx
+++ src/rock_paper_scissors_assets/src/components/Logo.tsx
@@ -1,30 +1,41 @@
 import React from "react";
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
+import {tada} from 'react-animations';
+
+// entry animation for the logo
+const logoAnim = keyframes`${tada}`;
 
 // The logo contains styling and text for the logo
 const Logo = styled.div`
 
   // size font based on view height
   font-size: 8vh;
-  
+
+  // give us some space on top
+  padding-top: 2vh;
+
   // center align
   text-align: center;
-  
+
   // cool, comic book yellow
   color: var(--color-yellow);
-  
+
   // logo font or sans
   font-family: logo-font, sans-serif;
-  
+
   // this prevents the 'k' and 'r' from overlapping the commas
   letter-spacing: 4px;
-  
+
   // outline effect
   -webkit-text-stroke: 2px black;
-  
-  // shaddow effect
+
+  // shadow effect
   text-shadow: 2px 4px 0 black;
-  
+
+  // entry anim
+  animation: 1s ${logoAnim};
+
+  // name of the game
   &:after {
     content: 'Block, Paper, Scissors!'
   }
