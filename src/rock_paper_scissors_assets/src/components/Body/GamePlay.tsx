30b049abbd64165f528e490720fd793093691a9e
yes-but-also-no <16858421+yes-but-also-no@users.noreply.github.com>
2021-07-24 11:17:56 -0700

    setup basic moves

--- src/rock_paper_scissors_assets/src/components/Body/GamePlay.tsx
+++ src/rock_paper_scissors_assets/src/components/Body/GamePlay.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import {Col, Grid, Row} from "react-flexbox-grid";
 import Note from "../Controls/Note";
+import {Spacer} from "../Helpers";
+import Button from "../Controls/Button";
 
 // this is the main gameplay screen
 const GamePlay: React.FC = () => {
@@ -16,6 +18,37 @@ const GamePlay: React.FC = () => {
 
                     <strong>choose a move to play</strong>
                 </Note>
+            </Col>
+        </Row>
+
+        <Spacer />
+        <Spacer />
+
+        <Row center='xs'>
+            <Col xs={11} md={9}>
+                <Button color='red'>
+                    <strong>rock</strong>, the power move
+                </Button>
+            </Col>
+        </Row>
+
+        <Spacer />
+
+        <Row center='xs'>
+            <Col xs={11} md={9}>
+                <Button color='yellow'>
+                    <strong>paper</strong>, the safe bet
+                </Button>
+            </Col>
+        </Row>
+
+        <Spacer />
+
+        <Row center='xs'>
+            <Col xs={11} md={9}>
+                <Button color='purple'>
+                    <strong>scissors</strong>, the wildcard
+                </Button>
             </Col>
         </Row>
 
